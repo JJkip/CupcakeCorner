8da0f82a235b1e4554646b36c9158205bf908c20
jjkip <jjlangatjose@gmail.com>
2023-07-03 23:58:59 +0300

    Added checkout view

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -1,5 +1,5 @@
 //
-//  AddressView.swift
+//  CheckoutView.swift
 //  CupcakeCorner
 //
 //  Created by Joseph Langat on 03/07/2023.
@@ -7,16 +7,15 @@
 
 import SwiftUI
 
-struct AddressView: View {
-    @ObservedObject var order: Order
-    
+struct CheckoutView: View {
+    @ObservableObject var order: Order
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct AddressView_Previews: PreviewProvider {
+struct CheckoutView_Previews: PreviewProvider {
     static var previews: some View {
-        AddressView(order: Order())
+        CheckoutView(order: Order())
     }
 }

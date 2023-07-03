2aa52677ab65eff29837f3f30259182b7b985a77
jjkip <jjlangatjose@gmail.com>
2023-07-03 23:53:14 +0300

    Taking basic order details

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -6,125 +6,40 @@
 //
 
 import SwiftUI
-//import URLImage
-
-class User: ObservableObject, Codable {
-    
-    enum CodingKeys: CodingKey {
-        case name
-    }
-    
-    @Published var name = "JJ Kip"
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-    }
-}
-
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-    var releaseDate: String
-    var artworkUrl100: String
-    
-    enum CodingKeys: String, CodingKey {
-        case trackId, trackName, collectionName, releaseDate
-        case artworkUrl100 = "artworkUrl100"
-    }
-}
 
 struct ContentView: View {
-    @State private var results = [Result]()
-    @State private var username = ""
-    @State private var email = ""
+    @StateObject var order = Order()
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Username", text: $username)
-                TextField("Email", text: $email)
-            }
-            Section {
-                Button("Create account") {
-                    print("Creating account")
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
                 }
-            }
-            .disabled(disabledForm)
-        }
-        
-        List(results, id: \.trackId) { item in
-            HStack {
-                AsyncImage(url: URL(string: item.artworkUrl100)) { phase in
-                    if let disImage = phase.image {
-                        disImage
-                            .resizable()
-                        //                        .scaledToFit()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 100, height: 100)
-                    } else if phase.error != nil {
-                        Text("There was an error loading the image")
-                    } else {
-                        ProgressView()
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled.animation())
+                    
+                    
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
                     }
                 }
-//                .frame(width: 100, height: 100)
-                VStack(alignment: .leading) {
-                    
-                    Text(item.trackName)
-                        .font(.headline)
-                    Text(item.collectionName)
-                    Text(formatDate(item.releaseDate))
-                        .font(.callout)
-                        .foregroundColor(Color.gray)
+                Section {
+                    NavigationLink {
+                        AddressView(order: order)
+                    } label: {
+                        Text("Delivery details")
+                    }
                 }
             }
-            
-        }
-        .task {
-            await loadData()
-        }
-        
-    }
-    
-    var disabledForm: Bool {
-        username.count < 5 || email.count < 5
-    }
-    
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("Invalid data")
-        }
-    }
-    
-    func formatDate(_ dateString: String) -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-        
-        if let date = dateFormatter.date(from: dateString) {
-            dateFormatter.dateFormat = "dd/MM/yyyy"
-            return dateFormatter.string(from: date)
-        } else {
-            return ""
+            .navigationTitle("Cupcake Corner")
         }
     }
 }
